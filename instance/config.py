0dc48304651e3bcd6110d4cc63c2e8763e74df60
Bokono <cyrussimba@gmail.com>
2025-12-25 14:35:35 +0300

    Prepare repo for Koyeb + Neon DB (add template & gitignore) - 2025-12-25T11:35:09Z

--- instance/config.py
+++
@@ -1,4 +0,0 @@
-# Local instance config (development). DO NOT COMMIT to git; contents are local-only.
-DATABASE_URL = "postgresql://wperfumes_hfdk_user:7zfVME5Jn9Unb3mPgUwMchsvIzZAn5nY@dpg-d55932i4d50c739q8000-a.oregon-postgres.render.com/wperfumes_hfdk?sslmode=require"
-# Other local config (optional)
-SECRET_KEY = "replace-with-a-long-random-secret-key"
